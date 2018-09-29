fd2e4a7d07874f5e8115972d8413aa5b2bf91f2f
Hemang Jayswal <jayswal.hemang@yahoo.co.in>
2018-09-29 18:05:49 +0530

    Initial commit - setup React, webpack, and Babel

---
+++ README.md
@@ -0,0 +1,15 @@
+## Visiretail React JS
+
+## Technologies
+* Node JS (8.12.0)
+* NPM (6.4.1)
+* React JS (16.*)
+* Webpack (4.*)
+* Babel (7.*)
+
+## Commands
+* npm install
+* npm start
+
+## Url
+* http://localhost:8080
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+  })],
+  devServer: {
+    historyApiFallback: true
+  },
+  externals: {
+    // global app config object
+    config: JSON.stringify({
+      apiUrl: 'http://localhost:8080'
+    })
+  }
+}
