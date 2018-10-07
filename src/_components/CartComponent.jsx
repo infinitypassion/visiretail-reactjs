f595a2f5c1193b3eccacb69155892329ceb1679b
Hemang Jayswal <jayswal.hemang@yahoo.co.in>
2018-10-07 09:53:54 +0530

    Integrating home page - converted cart modal into react js. removed javascript code.

--- src/_components/CartComponent.jsx
+++ src/_components/CartComponent.jsx
@@ -2,20 +2,50 @@ import React from 'react';
 
 import { icBasketImg, festivitiesImg, saintValentinImg } from '../_helpers';
 
-import { opbasket, init } from '../_helpers/main';
+import { opbasket } from '../_helpers/main';
 
 class CartComponent extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      cartItems: [
+        {
+          itemId: 0,
+          itemName: 'Fêtes de fin d’années',
+          itemImg: festivitiesImg,
+          itemQuantity: 2
+        },
+        {
+          itemId: 1,
+          itemName: 'Saint Valentin',
+          itemImg: saintValentinImg,
+          itemQuantity: 1
+        }
+      ]
+    };
   }
 
   // Start -- React lifecycle methods
-  componentDidMount() {
-    init();
-  }
   // End -- React lifecycle methods
 
   // Start -- Custom methods
+  handleChange(e) {
+    console.log('handle change called')
+  }
+
+  decreaseQuantity(e) {
+    if (this.state.cartItems[e.currentTarget.dataset.id].itemQuantity > 0)
+      this.state.cartItems[e.currentTarget.dataset.id].itemQuantity--;
+
+    this.forceUpdate();
+  }
+
+  increaseQuantity(e) {
+    this.state.cartItems[e.currentTarget.dataset.id].itemQuantity++;
+
+    this.forceUpdate();
+  }
   // End -- Custom methods
 
   // Render
@@ -25,38 +55,30 @@ class CartComponent extends React.Component {
         <a href="javascript:void(0)" onClick={opbasket.bind(this)}>
           <div className="basket in">
             <img src={icBasketImg} alt="basket" className="img-responsive" />
-            <span className="noof_item">2</span>
+            <span className="noof_item">{this.state.cartItems.length}</span>
           </div>
         </a>
         <div className="basket_wrap" id="basket">
-          <p>2 articles ont été ajoutés à votre panier !</p>
+          <p>{this.state.cartItems.length} articles ont été ajoutés à votre panier !</p>
           <ul className="cart_item">
-            <li>
-              <div className="imgwrap">
-                <img src={festivitiesImg} alt="#" className="img-responsive" />
-              </div>
-              <div className="item_des">
-                <h6>Fêtes de fin d’années</h6>
-                <div className="quantity_count">
-                  <button className="decrease btn-number" title="Decrease Quantity" />
-                  <input type="text" className="quantity-amount qty" defaultValue={1} />
-                  <button className="increase btn-number" title="Increase Quantity" />
-                </div>
-              </div>
-            </li>
-            <li>
-              <div className="imgwrap">
-                <img src={saintValentinImg} alt="#" className="img-responsive" />
-              </div>
-              <div className="item_des">
-                <h6>Saint Valentin</h6>
-                <div className="quantity_count">
-                  <button className="decrease btn-number" title="Decrease Quantity" />
-                  <input type="text" className="quantity-amount qty" defaultValue={1} />
-                  <button className="increase btn-number" title="Increase Quantity" />
-                </div>
-              </div>
-            </li>
+            {
+              this.state.cartItems.map((item, key) => {
+                return (
+                  <li key={key}>
+                    <div className="imgwrap">
+                      <img src={item.itemImg} alt="#" className="img-responsive" />
+                    </div>
+                    <div className="item_des">
+                      <h6>{item.itemName}</h6>
+                      <div className="quantity_count">
+                        <button className="decrease btn-number" title="Decrease Quantity" onClick={this.decreaseQuantity.bind(this)} data-id={item.itemId} />
+                        <input type="text" className="quantity-amount qty" value={item.itemQuantity} onChange={this.handleChange.bind(this)} />
+                        <button className="increase btn-number" title="Increase Quantity" onClick={this.increaseQuantity.bind(this)} data-id={item.itemId} />
+                      </div>
+                    </div>
+                  </li>);
+              })
+            }
           </ul>
           <a href="payment.html" className="btn-primary">PAIEMENT</a>
         </div>
--- src/_helpers/main.js
+++ src/_helpers/main.js
@@ -10,41 +10,6 @@ export function opbasket(event) {
 
   document.getElementsByClassName('prfile_wrap')[0].classList.remove('open');
 }
-
-// Start increase / decrease qty ----------------------------------------
-var value, quantity;
-export function createBindings(quantityContainer) {
-  var quantityAmount = quantityContainer.getElementsByClassName('quantity-amount')[0];
-  var increase = quantityContainer.getElementsByClassName('increase')[0];
-  var decrease = quantityContainer.getElementsByClassName('decrease')[0];
-  increase.addEventListener('click', function () { increaseValue(quantityAmount); });
-  decrease.addEventListener('click', function () { decreaseValue(quantityAmount); });
-}
-export function init() {
-  quantity = document.getElementsByClassName('quantity_count')
-  for (var i = 0; i < quantity.length; i++) {
-    createBindings(quantity[i]);
-  }
-}
-
-export function increaseValue(quantityAmount) {
-  value = parseInt(quantityAmount.value, 10);
-
-  console.log(quantityAmount, quantityAmount.value);
-
-  value = isNaN(value) ? 0 : value;
-  value++;
-  quantityAmount.value = value;
-}
-
-export function decreaseValue(quantityAmount) {
-  value = parseInt(quantityAmount.value, 10);
-
-  value = isNaN(value) ? 0 : value;
-  if (value > 0) value--;
-
-  quantityAmount.value = value;
-}
 /** End CART related *********************************************************************************************** */
 
 /** Start MENU related ******************************************************************************************* */
