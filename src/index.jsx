ab440392685424d275bfd67f1bbe18d66e6ca2e8
Hemang Jayswal <jayswal.hemang@yahoo.co.in>
2018-10-20 00:38:20 +0530

    fixed mobile view issue

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,9 @@ import { render } from 'react-dom';
 
 // Theme Styles
 import './_assets/css/animate.css';
-import './_assets/css/hack.css';
-import './_assets/css/responsive.css';
 import './_assets/css/style.css';
+import './_assets/css/responsive.css';
+import './_assets/css/hack.css';
 
 // App Component
 import { App } from './App';
